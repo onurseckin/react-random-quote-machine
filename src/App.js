65ea0ae22a509dba423b33877c3cb3ad7531e740
Onur Seckin Senoglu <onurseckinsenoglu@gmail.com>
2020-12-17 20:31:10 -0800

    colors added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Button, Container, Typography } from '@material-ui/core'
+import TwitterIcon from '@material-ui/icons/Twitter'
+import FormatQuoteIcon from '@material-ui/icons/FormatQuote'
 
+import getRandomColor from './Color'
 import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles({
   backgroundContainer: {
-    background: 'grey no-repeat center center fixed',
+    background: `${getRandomColor()} no-repeat center center fixed`,
     backgroundSize: 'cover',
-    minWidth: '100vh',
-    minHeight: '100vh',
-    width: '100vh',
-    height: '100vh',
+    minWidth: '100%',
+    minHeight: '100%',
+    width: '100%',
+    height: '100%',
+    border: '1px solid rgb(20.8%, 53.4%, 20.8%)',
   },
   button: {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -24,19 +28,35 @@ const useStyles = makeStyles({
     flexFlow: 'column wrap',
     margin: '20% auto',
     width: '50%',
-    height: '30%',
+    height: '40%',
     border: '1px solid grey',
+    borderRadius: '10px',
     background: 'white',
-    padding: 20,
+    padding: '40px 20px',
+    overflow: 'visible',
   },
   text: {},
+  quoteIcon: {
+    fontSize: 70,
+    height: 48,
+  },
   author: {
     alignSelf: 'flex-end',
-    width: 30,
   },
   buttonContainer: {
     display: 'flex',
-    flexFlow: 'row no-wrap',
+    flexFlow: 'row nowrap',
+    justifyContent: 'space-between',
+    border: '1px solid red',
+    minHeight: 48,
+    margin: 0,
+    padding: 0,
+    marginTop: 30,
+  },
+  tweetQuote: {
+    width: 20,
+    border: '1px solid blue',
+    padding: '0 20px',
   },
 })
 
@@ -47,12 +67,15 @@ function App() {
     <Container id='background-container' className={classes.backgroundContainer}>
       <Container id='quote-box' className={classes.quoteBox}>
         <Typography id='text' variant='h2'>
-          Quote Text Here
+          <FormatQuoteIcon className={classes.quoteIcon}></FormatQuoteIcon> Quote Text Here
         </Typography>
-        <Typography id='author' variant='body1' className={classes.author}>
+        <Typography id='author' variant='h6' className={classes.author}>
           Author
         </Typography>
         <Container id='button-container' className={classes.buttonContainer}>
+          <a id='tweet-quote' className={classes.tweetQuote} target='_top' href='https://www.twitter.com/intent/tweet'>
+            <TwitterIcon></TwitterIcon>
+          </a>
           <Button id='new-quote' className={classes.button} variant='contained' color='primary'>
             New Quote
           </Button>
